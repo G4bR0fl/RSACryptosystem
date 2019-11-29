71d6c5fcc896f160ce33bacc5953a87f1b39b3da
gabrielbessa <gabrielbessa@github.com>
2019-11-29 01:02:38 -0300

    Flawless, se rolar de meter um network agr vai ser doido

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 import sympy
 import numpy
 import random
+import time
 
 def xgcd(a, b):
     """return (g, x, y) such that a*x + b*y = g = gcd(a, b)"""
@@ -19,21 +20,21 @@ def mulinv(a, b):
 
 def generateKey():
 
-    p = sympy.randprime(10**0, 10**3)
+    p = sympy.randprime(10**0, 10**3) 
     q = p
 
     while p == q:
         q = sympy.randprime(10**0, 10**3)
     n = p*q
     phi_n = (p-1)*(q-1)
-    print("p =", p, "q =", q, "n =", n, "phi_n =", phi_n)
+    # print("p =", p, "q =", q, "n =", n, "phi_n =", phi_n)
     e = random.randrange(1, phi_n)
     g, _, _ = xgcd(e, phi_n)
     while g != 1:
         e = random.randrange(1, phi_n)
         g, _, _ = xgcd(e, phi_n)
     d = mulinv(e, phi_n)
-    print("p =", p, "q =", q, "n =", n, "phi_n =", phi_n, "e =", e, "d =", d)
+    # print("p =", p, "q =", q, "n =", n, "phi_n =", phi_n, "e =", e, "d =", d)
     
     return (p, q, n, e, phi_n, d)
 
@@ -41,22 +42,24 @@ def generateKey():
 def encrypt(message, e, n):
     public_part1, public_part2 = e, n 
     cipher = [(ord(char) ** public_part1) % public_part2 for char in message]
-    print(cipher)
     return cipher
 
 def decrypt(cipher, d, n):
     private_part1, private_part2 = d, n
     plaintext = [chr((char ** private_part1) % private_part2) for char in cipher]
     return ''.join(plaintext)
-
+print("Gerenating first key...")
+time.sleep(0.5)
 key1 = generateKey()
 print("First key generated")
+print("Gerenating second key...")
+time.sleep(0.5)
 key2 = generateKey()
 print("Second key generated")
-msg = input("Entra com a string:\n")
+msg = input("Insira um texto para ser encriptado:\n")
 ascii_encoding = [ord(char) for char in msg]
-print("Mensagem encriptada: ")
+print("Mensagem encriptada:")
 cifra1 = encrypt(msg, key1[3], key1[2])
 print(''.join(map(str,cifra1)))
 decrypted_msg = decrypt(cifra1, key1[5], key1[2])
-print("Mensagem desencriptada: ", decrypted_msg)
+print("Mensagem desencriptada:", decrypted_msg)
